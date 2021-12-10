db7550f857a5e4aa89baf15d8b66ad9cb3d04842
Ajith Kumar <ajithkumar@xmediasolution.com>
2021-12-10 10:51:45 +0530

    initial commit

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBw9MYTqaEfD9PlMEPxxzdKk-d4dj2ZtXE",
+  authDomain: "expense-2-e7a4d.firebaseapp.com",
+  databaseURL: "https://expense-2-e7a4d-default-rtdb.firebaseio.com",
+  projectId: "expense-2-e7a4d",
+  storageBucket: "expense-2-e7a4d.appspot.com",
+  messagingSenderId: "356248547728",
+  appId: "1:356248547728:web:10b7ebac110ad18a4f4315",
+  measurementId: "G-E17F7YHBY4"
+};
